1b17c30400fbddcefcbab1e7db749d3ff443a011
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-23 21:46:02 +0200

    Written some initial code for the parser and the console app

--- MathParser.Console/Program.cs
+++ MathParser.Console/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace MathParser.ConsoleApp
+﻿using MathParser.Core;
+
+namespace MathParser.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi mom");
+            Parser parser = new();
+            parser.Constants.Add("pi", Math.PI);
+            parser.Functions.Add("square", new MathFunction(new string[] { "x" }, "x*x"));
+            parser.Functions.Add("add", new MathFunction(new string[] { "x", "y" }, "x + y"));
+
+            while (true)
+            {
+                Console.Write("Enter an expression: ");
+                string? userExpression = Console.ReadLine();
+
+                if (userExpression == null)
+                {
+                    Console.WriteLine("Nothing to parse :(");
+                    return;
+                }
+
+                try 
+                {
+                    double result = parser.Parse(userExpression);
+                    Console.WriteLine($"{userExpression} = {result}");
+                } 
+                catch(Exception ex)
+                {
+                    Console.WriteLine("An error occured");
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
